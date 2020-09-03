3165e0c52341a761989417742be3523785096c65
Ben Altman <altmanguitar@gmail.com>
2020-09-03 17:46:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.match(/[a-z])? true
+  elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP SONNY!"
 #  else
 #    return "HUH?! SPEAK UP SONNY!"
