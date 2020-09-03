e72e336eb54efa49e8a4905b5a15772997cf72b7
Ben Altman <altmanguitar@gmail.com>
2020-09-03 17:48:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != "#{phrase}".upcase
-    return "HUH?! SPEAK UP SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
 #  else
 #    return "HUH?! SPEAK UP SONNY!"
   end
